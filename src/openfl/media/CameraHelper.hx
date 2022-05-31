a69374e026823966c2c89590313a31b0b9948b77
Allan Dowdeswell <secrets@confidant.ca>
2022-05-31 11:12:57 -0600

    Added CameraHelper with a nextCamera function.

---
+++ src/openfl/media/CameraHelper.hx
@@ -0,0 +1,60 @@
+package openfl.media;
+
+import js.lib.Promise;
+import js.Browser;
+import js.html.VideoElement;
+import js.html.MediaStream;
+import js.html.MediaStreamTrack;
+import js.html.CanvasRenderingContext2D;
+import js.html.CanvasElement;
+
+using Camera.MediaDeviceInfo;
+using Camera.MediaDevices;
+using Camera.MediaDeviceKind;
+
+/**
+ * This class makes it easier to work with multiple cameras.
+ */
+
+class CameraHelper {
+	public static var instance(default, null):CameraHelper = new CameraHelper();
+    var mediaDevices:MediaDevices;
+
+	private function new(){
+        mediaDevices = Reflect.getProperty(Browser.navigator, 'mediaDevices');
+    }
+	/**
+	 * Returns the name of the next camera. 
+	 * @param currentCamera 
+	 * @param callback 
+     * 
+     * 
+        var jsCam:Camera;
+        var jsCamName:String = ""; //retain the name of the currently used camera
+        CameraHelper.instance.nextCamera(jsCamName,function(s:String){
+            trace("nextCamera name:"+s); //returns something like "Back Camera"
+            jsCam = Camera.getCamera(s);
+            jsCamName = s;
+        });
+	 */
+	public function nextCamera(currentCamera:String = "",callback:String->Void):Void {
+        mediaDevices.enumerateDevices().then(function(_devices:Array<MediaDeviceInfo>) {
+            var names:Array<String> = [];
+			for (device in _devices) {
+				if (device.kind == MediaDeviceKind.VIDEO_INPUT) {
+                    trace("device:"+device.label);
+					names.push(device.label);
+				}
+			}
+            var nextCameraIndex:Int = 0;
+            trace("test:"+names.indexOf(currentCamera)+":"+names.length);
+            if(currentCamera != "" && names.indexOf(currentCamera) < names.length-1){
+                nextCameraIndex = names.indexOf(currentCamera) + 1;
+            } 
+            callback(names[nextCameraIndex]);
+            
+		});
+
+    }
+
+}
